844ebb065ab3f6f3c451514d455cf270e3e1fa00
maryam khodaparast <khcomputergirl@gmail.com>
2020-09-19 15:54:25 +0430

    Setting up views and routes

--- routes/web.php
+++ routes/web.php
@@ -37,11 +37,11 @@ use Illuminate\Support\Facades\Route;
 /***
  * Database row sql queries
  */
-Route::get('/insert', function () {
-    DB::insert('insert into posts (title, content) values(?,?)', [
-        'Laravel is awesome with Edwin', 'laravel is the best thing that has happened period'
-    ]);
-});
+// Route::get('/insert', function () {
+//     DB::insert('insert into posts (title, content) values(?,?)', [
+//         'Laravel is awesome with Edwin', 'laravel is the best thing that has happened period'
+//     ]);
+// });
 //Route::get('/read',function (){
 //    $results = DB::select('select * from posts where id=? ',[1]);
 //    foreach($results as $post){
@@ -80,13 +80,13 @@ Route::get('/insert', function () {
 //
 //});
 //start:lecture 10.54
-Route::get('/basicInsert', function () {
-    $post = new Post;
-    $post->title = 'new Eloquent title';
-    $post->content = 'Wow! eloquent is really cool';
-    $post->save();
+// Route::get('/basicInsert', function () {
+//     $post = new Post;
+//     $post->title = 'new Eloquent title';
+//     $post->content = 'Wow! eloquent is really cool';
+//     $post->save();
 
-});
+// });
 //Route::get('/basicUpdate',function (){
 //    $post = Post::findOrFail(2);
 //    $post->title = 'new Eloquent title 2';
@@ -95,9 +95,9 @@ Route::get('/basicInsert', function () {
 //});
 //end:lecture 10.54
 //start lecture 10.55
-Route::get('/create', function () {
-    Post::create(['title' => 'the create method 1', 'content' => 'Wow! I\'m learning eloquent1']);
-});
+// Route::get('/create', function () {
+//     Post::create(['title' => 'the create method 1', 'content' => 'Wow! I\'m learning eloquent1']);
+// });
 //end lecture 10.55
 //start lecture 10.56
 //Route::get('/update',function (){
@@ -119,9 +119,9 @@ Route::get('/create', function () {
 //});
 //end lecture 10.57
 //start lecture 10.58
-Route::get('/softdelete', function () {
-    Post::find(1)->delete();
-});
+// Route::get('/softdelete', function () {
+//     Post::find(1)->delete();
+// });
 ////end lecture 10.58
 //Route::get('/readsoftdelete', function () {
 //    //return $post = Post::find(1);
@@ -132,74 +132,81 @@ Route::get('/softdelete', function () {
 //Route::get('/restore',function (){
 //    Post::withTrashed()->where('is_admin',0)->restore();
 //});
-Route::get('/forcedelete',function (){
-    Post::onlyTrashed()->where('is_admin',0)->forceDelete();
-});
+// Route::get('/forcedelete',function (){
+//     Post::onlyTrashed()->where('is_admin',0)->forceDelete();
+// });
 /***
  * ELOQUENT RELATIONSHIPS
  */
 //One to One relationship
-Route::get('/user/{id}/post',function ($id){
-   return User::find($id)->post->content;
-});
-Route::get('/post/{id}/user',function ($id){
-    return Post::find($id)->user->name;
-});
+// Route::get('/user/{id}/post',function ($id){
+//    return User::find($id)->post->content;
+// });
+// Route::get('/post/{id}/user',function ($id){
+//     return Post::find($id)->user->name;
+// });
 //One to many relationship
-Route::get('/posts',function (){
-    $user = User::find(1);
-    foreach($user->posts as $post){
-        echo  $post->title."<br>";//if we use return just one of the posts is returned
-    }
-});
+// Route::get('/posts',function (){
+//     $user = User::find(1);
+//     foreach($user->posts as $post){
+//         echo  $post->title."<br>";//if we use return just one of the posts is returned
+//     }
+// });
 //Many to Many relationship
-Route::get('/user/{id}/role',function ($id){
+//Route::get('/user/{id}/role',function ($id){
 //    $user = User::find($id);
 //    foreach($user->roles as $role){
 //        return $role->name;
 //    }
-    return User::find($id)->roles()->orderBy('id','desc')->get();
-});
+   // return User::find($id)->roles()->orderBy('id','desc')->get();
+//});
 //Accessing the intermediate table(user_role)/pivot
-Route::get('/user/pivot',function (){
-    $user = User::find(1);
-    foreach($user->roles as $role){
-       return $role->pivot->created_at;
-    }
-});
-Route::get('/user/country',function (){
-    $country = Country::find(1);
-    foreach($country->posts as $post){
-        return $post->title;
-    }
-});
+// Route::get('/user/pivot',function (){
+//     $user = User::find(1);
+//     foreach($user->roles as $role){
+//        return $role->pivot->created_at;
+//     }
+// });
+// Route::get('/user/country',function (){
+//     $country = Country::find(1);
+//     foreach($country->posts as $post){
+//         return $post->title;
+//     }
+// });
 //polymorphic relations
-Route::get('/user/{id}/photos',function($id){
-    $user = User::find($id);
-    foreach($user->photos as $photo){
-        echo $photo->path."<br>";
-    }//we can change user to post because we have photos relation for posts too...
-});
+// Route::get('/user/{id}/photos',function($id){
+//     $user = User::find($id);
+//     foreach($user->photos as $photo){
+//         echo $photo->path."<br>";
+//     }//we can change user to post because we have photos relation for posts too...
+// });
 //polymetric relations reverse
-Route::get('/photo/{id}/post',function($id){
-    $photo = Photo::findOrFail($id);
-    return $photo->imageable;//with this you can find a post or a user that these photo belongs to
+// Route::get('/photo/{id}/post',function($id){
+//     $photo = Photo::findOrFail($id);
+//     return $photo->imageable;//with this you can find a post or a user that these photo belongs to
 
-});
+// });
 
 //Polymorphic many to  many
-Route::get('/post/tag',function(){
-    $post = Post::find(1);
-    foreach($post->tags as $tag){
-        echo $tag->name;
-    }
-});
-Route::get('/tag/post',function(){
-   $tag = Tag::find(2);
-   foreach($tag->posts as $post){
-       echo $post->title;
-   }
-});
+// Route::get('/post/tag',function(){
+//     $post = Post::find(1);
+//     foreach($post->tags as $tag){
+//         echo $tag->name;
+//     }
+// });
+// Route::get('/tag/post',function(){
+//    $tag = Tag::find(2);
+//    foreach($tag->posts as $post){
+//        echo $post->title;
+//    }
+// });
+
+/*
+|--------------------------------------------------------------------------
+| Crud Application
+|-------------------------------------------------------------------------
+*/
+Route::resource('/posts','PostsController');
 
 
 
