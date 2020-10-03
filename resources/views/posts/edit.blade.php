c38f2ce14a68560aa02107ff73f8e344970ac849
maryam khodaparast <khcomputergirl@gmail.com>
2020-10-03 21:11:59 +0330

    Preparing the edit view and setting the token

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 @section('content')
     <h1>Edit Post</h1>
-    <form action="{{ route('posts.update') }}" method="POST">
+    <form action="/posts/{{ $post->id }}" method="POST">
         @csrf
-        <input type="text" name="title" placeholder="Enter title">
+        <input type="hidden" name="_method" value="PUT">
+        <input type="text" name="title" value="{{ $post->title }}" placeholder="Enter title">
         <input type="submit" name="submit">
     </form>
 @endsection
