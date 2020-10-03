91dd67b3b248a662039d6e931dd98f0b0faef74a
maryam khodaparast <khcomputergirl@gmail.com>
2020-10-03 20:28:10 +0330

    Setting up the controller and views 18-124

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
   <form action="/posts" method="POST">
+      @csrf
       <input type="text" name="title" placeholder="Enter title">
       <input type="submit" name="submit">
   </form>
