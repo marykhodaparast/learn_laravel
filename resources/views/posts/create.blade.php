1da56ec75655d7079db247ed6186ed3b11cccf85
maryam khodaparast <khcomputergirl@gmail.com>
2020-10-04 13:49:33 +0330

    Installing package and testing 19-132

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 @section('content')
     <h1>Create Post</h1>
-    <form action="/posts" method="POST">
+    {{-- <form action="/posts" method="POST"> --}}
+        {!! Form::open() !!}
         @csrf
         <input type="text" name="title" placeholder="Enter title">
         <input type="submit" name="submit">
