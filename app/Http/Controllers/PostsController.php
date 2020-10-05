9525606622e0164a41d95a0f1e92e28d4185153c
maryam khodaparast <khcomputergirl@gmail.com>
2020-10-05 15:12:01 +0330

    Query Scope 20-143

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -15,7 +15,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::hello();
+        //dd($posts);
         return view('posts.index',compact('posts'));
     }
 
--- app/Post.php
+++ app/Post.php
@@ -29,4 +29,7 @@ class Post extends Model
     public function tags(){
         return $this->morphToMany('App\Tag','taggable');
     }
+    public static function scopeHello($query){
+        return $query->orderBy('id','desc')->get();
+    }
 }
